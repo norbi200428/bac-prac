6cc6b5b53a66d10b39898181c767555763f8a1ac
norbi200428 <91279315+norbi200428@users.noreply.github.com>
2023-02-07 08:12:41 +0200

    Add files via upload

---
+++ 2022_elo_III_2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n , t[100][100];
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin >> t[i][j];
+        }
+    }
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+
+        }
+    }
+    return 0;
+}
